77a6df49b5959506ac9e7ac39ee53df57e04fb25
ZhouTian0716 <egglord0716@gmail.com>
2022-11-12 01:10:14 +1100

    charpter15 done ready for deploy

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,14 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+const databaseName = "mongo-tutorial-db";
+const mongoUserName = process.env.DATABASE_USER;
+const mongoUserPassword = process.env.DATABASE_USER_PWD;
+const connectionURI = `mongodb+srv://${mongoUserName}:${mongoUserPassword}@cluster0.oomzs.mongodb.net/${databaseName}?retryWrites=true&w=majority`;
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DATABASE_URI, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        });
-    } catch (err) {
-        console.error(err);
-    }
-}
+  try {
+    await mongoose.connect(connectionURI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log(`Hello ${mongoUserName}, welcome to your connected mongo database name : ${databaseName}`)
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-module.exports = connectDB
+module.exports = connectDB;
